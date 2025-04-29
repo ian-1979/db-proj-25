474e891b737c24a226f83299c02e8bb0baebffdf
J-Mill3 <j.miller00579@gmail.com>
2025-04-28 20:44:45 -0400

    Updating the sample_data.sql queries to add data that works with the newest schema (dbdatabase1.sql).

--- dnd_flask_app/sql/sample_data.sql
+++ dnd_flask_app/sql/sample_data.sql
@@ -1,78 +1,76 @@
--- you should import this into your XAMPP
+-- Adds sample data that is up to data with the dbdatabase1.sql schema
 
-
--- Sample Campaigns
-INSERT INTO campaign (name, description, campaign_image_path) VALUES
-('Curse of the Underdark', 'A campaign of ancient ruins, lurking monsters, and lost magic.', NULL),
-('Rise of the Storm Queen', 'An elemental upheaval threatens the world. Heroes must rise.', NULL);
-
--- Sample Notecards
+-- Additional Notecards
 INSERT INTO notecard (name, type, text, campaign_id, public, note_image_path) VALUES
-('Alaric the Brave', 'character', 'A noble paladin from the northern provinces.', 1, 1, NULL),
-('Cave of Echoes', 'location', 'A mysterious cavern rumored to be cursed.', 1, 1, NULL),
-('Grimfang', 'monster', 'A fearsome orc chieftain wielding a cursed axe.', 1, 1, NULL),
-('Elandra the Stormcaller', 'character', 'A tempest sorceress of unmatched fury.', 2, 1, NULL),
-('Skyspire', 'location', 'A floating citadel among the storm clouds.', 2, 1, NULL),
-('Thunderbeast', 'monster', 'A massive elemental beast with lightning breath.', 2, 1, NULL);
+('Sir Gareth the Bold', 'character', 'A knight famed for his dragon-slaying.', 1, 1, NULL),
+('Whispering Woods', 'location', 'A dense, misty forest filled with strange magic.', 1, 1, NULL),
+('Zara the Silent', 'npc', 'A rogue information broker working the shadows.', 1, 1, NULL),
+('The Crimson Maw', 'monster', 'A feral vampire lord that haunts ancient ruins.', 1, 1, NULL),
+('Temple of the Sun', 'location', 'An ancient abandoned temple devoted to the Sun God.', 2, 1, NULL),
+('Storm Serpent', 'monster', 'A gigantic snake that channels storms through its scales.', 2, 1, NULL),
+('High Priestess Lyra', 'npc', 'A leader of the Sun Temple cult, keeper of forbidden secrets.', 2, 1, NULL);
 
--- Sample Entities
+-- Additional Entities
 INSERT INTO entity (note_id, entity_type, level, class, race, strength, dexterity, constitution, intelligence, wisdom, charisma) VALUES
-(1, 'character', 5, 1, 'Human', 16, 12, 14, 10, 13, 15),
-(3, 'monster', 3, NULL, 'Orc', 18, 10, 16, 8, 9, 6),
-(4, 'character', 7, 2, 'Elf', 10, 14, 12, 18, 11, 17),
-(6, 'monster', 6, NULL, 'Elemental', 20, 12, 18, 8, 10, 5);
+(7, 'character', 6, 'Fighter', 'Human', 18, 11, 16, 10, 12, 13),
+(8, 'location', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(9, 'npc', 5, 'Rogue', 'Halfling', 8, 18, 10, 14, 12, 16),
+(10, 'monster', 8, NULL, 'Vampire', 20, 18, 16, 14, 16, 18),
+(11, 'location', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(12, 'monster', 7, NULL, 'Dragon', 22, 16, 20, 12, 14, 10),
+(13, 'npc', 9, 'Cleric', 'Elf', 8, 12, 14, 16, 18, 12);
 
--- Sample Features
+-- Additional Features
 INSERT INTO feature (note_id, modifier_text) VALUES
-(1, 'Lay on Hands: Restore 25 HP per day.'),
-(4, 'Storm Aura: Deal 1d6 lightning damage to nearby enemies each turn.'),
-(6, 'Thunder Roar: Enemies within 30ft must pass CON save or be stunned.');
+(7, 'Dragon Slayer: +5 bonus when fighting dragons.'),
+(9, 'Sneak Attack: +3d6 bonus damage when attacking unaware enemies.'),
+(10, 'Life Drain: Victims must save or lose max HP permanently.'),
+(13, 'Blessing of Light: Allies within 30 feet gain +2 AC.');
 
--- Feature Relationships
+-- More Entity-Feature Relationships
 INSERT INTO entity_feature (entity_id, feature_id) VALUES
-(1, 1),
-(3, 2),
-(4, 2),
-(6, 3);
+(7, 4),
+(9, 5),
+(10, 6),
+(13, 7);
 
--- Sample Spells
+-- Additional Spells
 INSERT INTO spells (note_id, level, school, spell_text, damage_type, damage, save) VALUES
-(4, 3, 'Evocation', 'A bolt of lightning strikes from the sky, dealing 4d10 damage.', 2, 10, 1),
-(6, 2, 'Conjuration', 'Summons a burst of thunder to knock back enemies.', 1, 6, 1);
+(13, 3, 'Divination', 'Reveals hidden traps and invisible creatures.', 'Radiant', 0, 0),
+(12, 4, 'Evocation', 'Unleashes a cone of storm energy dealing 8d6 lightning damage.', 'Lightning', 8, 1);
 
 -- Spell Relationships
 INSERT INTO entity_spell (entity_id, spell_id) VALUES
-(4, 1),
-(6, 2);
+(13, 3),
+(12, 4);
 
--- Tags
-INSERT INTO tag (name) VALUES ('underground'), ('boss'), ('paladin'), ('storm'), ('mage'), ('location'), ('elemental');
+-- More Tags
+INSERT INTO tag (name) VALUES
+('vampire'), ('temple'), ('forest'), ('rogue'), ('storm');
 
--- Entity-Tag Relationships
-INSERT INTO entity_tag (entity_id, tag_id) VALUES
-(1, 3), -- Alaric → paladin
-(2, 1), -- Grimfang → underground
-(2, 2), -- Grimfang → boss
-(4, 4), -- Elandra → storm
-(4, 5), -- Elandra → mage
-(6, 2), -- Thunderbeast → boss
-(6, 7); -- Thunderbeast → elemental
+-- More Notecard-Tag Relationships
+INSERT INTO notecard_tag (note_id, tag_id) VALUES
+(8, 3), -- Whispering Woods → forest
+(9, 4), -- Zara → rogue
+(10, 1), -- Crimson Maw → vampire
+(11, 2), -- Temple of the Sun → temple
+(12, 5); -- Storm Serpent → storm
 
--- Users
+-- Additional Users
 INSERT INTO user (username, password, profile_image_path) VALUES
-(101, 'hashedpass1', NULL),
-(102, 'hashedpass2', NULL);
+('player3', 'hashedpass3', NULL),
+('player4', 'hashedpass4', NULL);
 
 -- User-Campaign Relationships
-INSERT INTO user_campaign (user_id, campaign_id) VALUES
-(101, 1),
-(102, 2);
+INSERT INTO user_campaign (user_id, campaign_id, role) VALUES
+(3, 1, 'player'),
+(4, 2, 'player');
 
--- User-Note Relationships (ownership or authorship)
-INSERT INTO user_note (user_id, note_id) VALUES
-(101, 1),
-(101, 2),
-(101, 3),
-(102, 4),
-(102, 5),
-(102, 6);
+-- User-Note Relationships
+INSERT INTO user_note (user_id, note_id, note_text) VALUES
+(3, 7, 'Dragon-slaying knight.'),
+(3, 8, 'Mysterious forest.'),
+(3, 9, 'Stealthy informant.'),
+(4, 10, 'Vampire boss.'),
+(4, 11, 'Ancient sun temple.'),
+(4, 12, 'Massive storm snake.');
